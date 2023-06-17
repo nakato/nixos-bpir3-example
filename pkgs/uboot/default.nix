10c85e46844fa9874d9307d20ee1866ba1765e5e
Sachi King <nakato@nakato.io>
2023-06-17 14:31:25 +1000

    Overhall BPiR3 uboot
    No more messing with include/configs/board.h for getting basic boot up
    and running.  Use a custom named plain-text environment file to setup
    required environment for extlinux booting to work.  This means no patch
    conflicts!  Instruct bootstd boot source through a device-tree include.
    
    Also, delete a ton of config values at the same time as part of this
    large overhall.
    
    Also DTB relocation is working!  So the config should actually be
    somewhat okay now.

--- pkgs/uboot/default.nix
+++ pkgs/uboot/default.nix
@@ -6,7 +6,6 @@
 }:
 let
   extraPatches = [
-      ./mt7986-default-bootcmd.patch
       ./mt7986-persistent-mac-from-cpu-uid.patch
       ./mt7986-persistent-wlan-mac-from-cpu-uid.patch
     ];
@@ -16,29 +15,29 @@ in
     defconfig = "mt7986a_bpir3_sd_defconfig";
     extraMeta.platforms = ["aarch64-linux"];
     extraPatches = extraPatches;
+    postPatch = ''
+      cp ${./mt7986-nixos.env} board/mediatek/mt7986/mt7986-nixos.env
+    '';
     extraConfig = ''
+      CONFIG_AUTOBOOT=y
+      CONFIG_BOOTDELAY=1
+      CONFIG_USE_BOOTCOMMAND=y
+      # Use bootstd and bootflow over distroboot for extlinux support
       CONFIG_BOOTSTD_DEFAULTS=y
       CONFIG_BOOTSTD_FULL=y
       CONFIG_CMD_BOOTFLOW_FULL=y
-      # Expo for bootflow menu
-      CONFIG_EXPO=y
-      CONFIG_DISTRO_DEFAULTS=y
-      CONFIG_CMD_SYSBOOT=y
-      CONFIG_AUTOBOOT=y
-      CONFIG_BOOTDELAY=2
-      CONFIG_USE_BOOTCOMMAND=y
+      CONFIG_DEVICE_TREE_INCLUDES="${./mt7986-mmcboot.dtsi}"
+      CONFIG_ENV_SOURCE_FILE="mt7986-nixos"
+      # Unessessary as it's not actually used anywhere, value copied verbatum into env
       CONFIG_DEFAULT_FDT_FILE="mediatek/mt7986a-bananapi-bpi-r3.dtb"
       # Big kernels
       CONFIG_SYS_BOOTM_LEN=0x6000000
-      CONFIG_CMD_ERASEENV=y
-      CONFIG_CMD_UNLZ4=y
-      CONFIG_CMD_UNZIP=y
-      CONFIG_CMD_CAT=y
-      CONFIG_XXHASH=y
+      # Disable saving env, it isn't tested and probably doesn't work.
+      CONFIG_ENV_IS_NOWHERE=y
       CONFIG_LZ4=y
       CONFIG_BZIP2=y
       CONFIG_ZSTD=y
-      # Set the envvars: board_late_init
+      # The following are used in the tooling to fixup MAC addresses
       CONFIG_BOARD_LATE_INIT=y
       CONFIG_SHA1=y
       CONFIG_OF_BOARD_SETUP=y
