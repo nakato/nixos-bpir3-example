4210480bdebbf3a7953e22d5d9f183f47b725bff
Sachi King <nakato@nakato.io>
2023-07-06 15:06:41 +1000

    Add "MII" to kernel modules
    If your SFP is MII and it is plugged in during a warm-boot then it'll
    break on boot untill you re-plug it or cold boot.  By including MII the
    SFP probes sucessfully and works on warm and cold boots.

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
               # They fail to load properly, leaving the system without working ethernet, they'll oops on
               # remove. MTK-DSA parts and PCIe were observed to do this.
               boot.initrd.includeDefaultModules = false;
-              boot.initrd.kernelModules = [ "rfkill" "cfg80211" "mt7915e" ];
+              boot.initrd.kernelModules = [ "rfkill" "cfg80211" "mt7915e" "mii" ];
               boot.kernelParams = [ "console=ttyS0,115200" ];
               hardware.enableRedistributableFirmware = true;
               # Wireless hardware exists, regulatory database is essential.
