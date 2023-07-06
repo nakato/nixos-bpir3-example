50142ae6f584ca06bce301181732dab035b7ef1a
Sachi King <nakato@nakato.io>
2023-07-06 14:47:36 +1000

    Create a full-featured kernel by default
    linuxPackages_bpir3_minimal exists if the distro-standard style kernel
    with mediatek options built-in.
    
    This larger kernel has less suprises, as the minimal kernel is all about
    compile time and not compatability.
    
    Compiling this full-featured kernel takes about 90 minutes on a RK3588s
    with 16GB of RAM, /tmp as a 32GB tmpfs, 8GB of zram SWAP, and 16GB SWAP
    on an eMMC.  ZRAM was hit, but SWAP was not.
    
    The same hardware compiles the minimal kernel in under 15 mintues.
    
    If you're not actively testing patches in which case you're bottlenecked
    by compiler throughput you should probably take the full kernel as it
    won't need to build that often, and as such if it takes a while to
    build, then it takes a while to build.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,5 +7,5 @@ in
 {
   inherit (uboot) ubootBananaPiR3;
   inherit (atf) armTrustedFirmwareMT7986;
-  inherit (linux) linuxPackages_bpir3;
+  inherit (linux) linuxPackages_bpir3 linuxPackages_bpir3_minimal;
 }
--- pkgs/linux.nix
+++ pkgs/linux.nix
@@ -1,51 +1,101 @@
-{ fetchurl
-, fetchpatch
-, lib
+{ lib
 , linuxKernel
-, buildLinux
-, recurseIntoAttrs
+, linux_6_4
 , copyPathToStore
 , ...
 }:
 let
-  # customPackage closure copied from: nixpkgs/pkgs/top-level/linux-kernels.nix
-  customPackage = { version, src, modDirVersion ? lib.versions.pad 3 version, configfile, allowImportFromDerivation ? true, kernelPatches }:
-    recurseIntoAttrs (linuxKernel.packagesFor (linuxKernel.manualConfig {
-      inherit version src modDirVersion configfile allowImportFromDerivation kernelPatches;
-    }));
+  kernelPatches = [
+    {
+      # Cold boot PCIe/NVMe have stability issues.
+      # See: https://forum.banana-pi.org/t/bpi-r3-problem-with-pcie/15152
+      #
+      # FrankW's first patch added a 100ms sleep, this was rejected upstream.
+      # Jianjun posted a patch to the forum for testing, and it appears to me
+      # to have accidentally missed a write to the registers between the two
+      # sleeps.  This version is modified to include the write, and results
+      # in the PCI bridge appearing reliably, but not the NVMe device.
+      #
+      # Without this patch, the PCI bridge is not present, and rescan does
+      # not discover it.  Removing the bridge and then rescanning repeatably
+      # gets the NVMe working on cold-boot.
+      name = "PCI: mediatek-gen3: handle PERST after reset";
+      patch = ./linux-mtk-pcie.patch;
+    }
+  ];
 
-  linux_bpir3 = customPackage {
-    version = "6.4.0";
-    modDirVersion = "6.4.0";
-
-    src = fetchurl {
-      url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.4.tar.xz";
-      hash = "sha256-j6BYjwws7KRMrHeg45ukjJ8AprncaXYcAqXT76yNp/M=";
+  linuxPackages_bpir3_minimal = let
+    base = linux_6_4;
+    bpir3_minimal = linuxKernel.customPackage {
+      inherit (base) version modDirVersion src;
+      # A working config cannot be built with structedExtraConfig...
+      # To disable, say, NET_DSA_TAG_BRCM, B53 needs to first be disabled, which has not yet been prompted for yet.
+      # To disable B53, B53_SRAB_DRIVER needs to be disabled, which is prompted afterwards.
+      configfile = copyPathToStore ./bpir3_kernel.config;
     };
-    kernelPatches = [
-      {
-        # Cold boot PCIe/NVMe have stability issues.
-        # See: https://forum.banana-pi.org/t/bpi-r3-problem-with-pcie/15152
-        #
-        # FrankW's first patch added a 100ms sleep, this was rejected upstream.
-        # Jianjun posted a patch to the forum for testing, and it appears to me
-        # to have accidentally missed a write to the registers between the two
-        # sleeps.  This version is modified to include the write, and results
-        # in the PCI bridge appearing reliably, but not the NVMe device.
-        #
-        # Without this patch, the PCI bridge is not present, and rescan does
-        # not discover it.  Removing the bridge and then rescanning repeatably
-        # gets the NVMe working on cold-boot.
-        name = "PCI: mediatek-gen3: handle PERST after reset";
-        patch = ./linux-mtk-pcie.patch;
-      }
-    ];
-    # A working config cannot be built with structedExtraConfig...
-    # To disable, say, NET_DSA_TAG_BRCM, B53 needs to first be disabled, which has not yet been prompted for yet.
-    # To disable B53, B53_SRAB_DRIVER needs to be disabled, which is prompted afterwards.
-    configfile = copyPathToStore ./bpir3_kernel.config;
+  in
+    bpir3_minimal;
+
+  linux_bpir3 = linux_6_4.override {
+    inherit kernelPatches;
+
+    # This will take ~22GB to build.  /tmp better be big.
+    structuredExtraConfig = with lib.kernel; {
+      # Disable extremely unlikely features to reduce build time and storage requirements
+      # DRM takes a substantual amount of storage during build
+      DRM = no;
+      SOUND = no;
+      # Where would you attach an IB interface?
+      INFINIBAND = lib.mkForce no;
+
+      # Build-in BPiR3 support, many misbehave when compiled as modules.
+      # Known problematic drivers are MT7530/DSA and PCIe.
+
+      # PCIe
+      PCIE_MEDIATEK = yes;
+      PCIE_MEDIATEK_GEN3 = yes;
+      # SD/eMMC
+      MTD_NAND_ECC_MEDIATEK = yes;
+      # Net
+      BRIDGE = yes;
+      HSR = yes;
+      NET_DSA = yes;
+      NET_DSA_TAG_MTK = yes;
+      NET_DSA_MT7530 = yes;
+      NET_VENDOR_MEDIATEK = yes;
+      PCS_MTK_LYNXI = yes;
+      NET_MEDIATEK_SOC_WED = yes;
+      NET_MEDIATEK_SOC = yes;
+      NET_MEDIATEK_STAR_EMAC = yes;
+      MEDIATEK_GE_PHY = yes;
+      # WLAN
+      WLAN = yes;
+      WLAN_VENDOR_MEDIATEK = yes;
+      MT76_CORE  = module;
+      MT76_LEDS = yes;
+      MT76_CONNAC_LIB = module;
+      MT7815E = module;
+      MT7986_WMAC = yes;
+      # Pinctrl
+      EINT_MTK = yes;
+      PINCTRL_MTK = yes;
+      PINCTRL_MT7986 = yes;
+      # Thermal
+      MTK_THERMAL = yes;
+      MTK_SOC_THERMAL = yes;
+      MTK_LVTS_THERMAL = yes;
+      # Clk
+      COMMON_CLK_MEDIATEK = yes;
+      COMMON_CLK_MEDIATEK_FHCTL = yes;
+      COMMON_CLK_MT7986 = yes;
+      COMMON_CLK_MT7986_ETHSYS = yes;
+      # other
+      MEDIATEK_WATCHDOG = yes;
+      REGULATOR_MT6380 = yes;
+    };
   };
 in
 {
-  linuxPackages_bpir3 = linux_bpir3;
+  linuxPackages_bpir3 = linuxKernel.packagesFor linux_bpir3;
+  inherit linuxPackages_bpir3_minimal;
 }
